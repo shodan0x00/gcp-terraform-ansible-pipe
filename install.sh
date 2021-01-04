93de2c71680a395d85e3ed17b80324890595b301
asborsiov <70656108+asborsiov@users.noreply.github.com>
2021-01-04 17:46:20 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ gcloud projects add-iam-policy-binding $id \
     --member="serviceAccount:acg-sg@$id.iam.gserviceaccount.com" \
     --role="roles/owner"
 
-gcloud iam service-accounts keys create ~/credentials.json \
+gcloud iam service-accounts keys create ~/bootstrap/credentials.json \
   --iam-account acg-sg@$id.iam.gserviceaccount.com
 
 b=$(gcloud alpha billing accounts list --uri)
@@ -22,7 +22,6 @@ b=$(gcloud alpha billing accounts list --uri)
 gcloud alpha billing accounts projects link $id --account-id $b
 
 gcloud services enable compute.googleapis.com
-
 
 apt-get update && apt-get install ansible -y
 
@@ -32,6 +31,8 @@ unzip -o terraform_0.14.3_linux_amd64.zip
 
 mv -f terraform /usr/bin
 
+cd bootstrap
+
 bucket=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${1:-32} | head -n 1)
 
 export GOOGLE_APPLICATION_CREDENTIALS=$(pwd)/credentials.json
@@ -40,10 +41,16 @@ sed -i -e "s/@project-name/\"$id\"/g" gcp.tfvars
 
 sed -i -e "s/@bucket-name/\"$bucket\"/g" gcp.tfvars
 
-sed -i -e "s/@bucket-name/\"$bucket\"/g" compute/compute.tf
+sed -i -e "s/@bucket-name/\"$bucket\"/g" compute/create-instances.tf
 
 sed -i -e "s/@bucket-name/\"$bucket\"/g" template/ansible_template.tf
 
-cd $HOME/gcp-terraform-ansible-pipe/remote-state && terraform init && terraform apply -var-file='/home/asborisov/gcp/gcp.tfvars' -auto-approve
+cd $HOME/bootstrap/remote-state && terraform init && terraform apply -var-file='/home/asborisov/gcp/gcp.tfvars' -auto-approve
 
-cd $HOME/gcp-terraform-ansible-pipe/compute && terraform init && terraform apply -var-file='/home/asborisov/gcp/gcp.tfvars' -auto-approve
+cd $HOME/bootstrap/compute && terraform init && terraform apply -var-file='/home/asborisov/gcp/gcp.tfvars' -auto-approve
+
+cd $HOME/bootstrap/template && terraform init && terraform apply -var-file='/home/asborisov/gcp/gcp.tfvars' -auto-approve
+
+echo "INSTALLATION DONE"
+
+pwd
