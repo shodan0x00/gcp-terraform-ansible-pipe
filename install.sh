1b10e6e645b993d789ece268ecb4313356a0ee24
shodan0x00 <shodanrc@gmail.com>
2021-01-20 05:05:44 +0000

    changed ansible.cfg

--- install.sh
+++ install.sh
@@ -73,8 +73,8 @@ cd /opt/bootstrap/template && terraform init && terraform apply -var-file='/opt/
 sleep 10
 
 #executing ansible inits
-cd /opt/bootstrap/ansible && ansible-playbook -i /opt/bootstrap/hosts airflow.yaml --private-key /opt/bootstrap/ssh-key
+cd /opt/bootstrap/ansible && ansible-playbook -i /opt/bootstrap/hosts playbooks/install_all.yaml --private-key /opt/bootstrap/ssh-key
 
-cd /opt/bootstrap/ansible && ansible-playbook -i /opt/bootstrap/hosts postgres-kafka-nifi.yaml --private-key /opt/bootstrap/ssh-key
+#cd /opt/bootstrap/ansible && ansible-playbook -i /opt/bootstrap/hosts postgres-kafka-nifi.yaml --private-key /opt/bootstrap/ssh-key
 
 echo "INSTALLATION DONE"
